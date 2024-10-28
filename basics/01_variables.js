75ea6cc11c758643f9c1f37c9adfe374593a7786
Krishma <krishmadeoju4@gmail.com>
2024-10-28 13:03:08 +0000

    variables and datatype

---
+++ basics/01_variables.js
@@ -0,0 +1,23 @@
+//variables
+
+const accountId = 144553
+let accountEmail = "krishma@gmail.com"
+var accountPassword = "123456"
+accountCity = "Dhulikhel"
+let accountState ;
+//accountId = 2 //not allowed
+
+accountEmail = "haga.com"
+accountPassword = "1452"
+accountCity = "Banepa"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS as newer version
+
+//alert (3+3)  //we are using node js not browser
+
+console.log(3
+    +3)         //code readability should be high
+
+console.log("krishma")
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined 
+//symbol => unique
+let name = "klrishma"
+let age = 22
+let isLoggedIn = false
+
+
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
