38953230601de949c94b0a562a5329f01bff9673
Krishma <krishmadeoju4@gmail.com>
2024-10-30 14:48:26 +0000

    conversion of Datatypes

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 1234
+
+//console.log(typeof score)
+//console.log(typeof (score))    //string
+
+const valueInNumber = Number(score)
+//console.log(valueInNumber)    //NaN
+//console.log(typeof valueInNumber)  //number
+
+
+//"33" => 33
+//"33abc" => NaN not a number
+//true => 1;  false => 0
+
+let isLoggedIn = "krishma"
+//console.log(typeof isLoggedIn)
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 => true; 0 =>false
+//"" => false
+//"krishma" => true
+
+let someNumber =  12
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
