6db5329c4409e813abd59649b4aecbf94b3205bb
Krishma <krishmadeoju4@gmail.com>
2024-11-19 13:41:35 +0000

    json talk

---
+++ basics2/05_objects.js
@@ -0,0 +1,24 @@
+//destructuring
+const course = {
+    courseName: "javaScript",
+    price: "1500",
+    courseInstructor: "krishma"
+}
+
+//course.courseInstructor
+
+const{courseInstructor:Instructor} = course
+console.log(Instructor);
+
+
+//APIs
+//JSON
+
+
+// {
+//     "namw": "krishma",
+//     "coursename ": "javaScript",
+//     "price ": "free"
+// }
+
+
